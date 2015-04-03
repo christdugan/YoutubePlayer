e3701e8dfecc77982ca848cbe534b7ff6bb11f53
Chris Dugan <chris.dugan@flybymedia.com>
2015-04-03 15:57:48 -0400

    Initial Commit

---
+++ YT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YT
+//
+//  Created by Christopher Dugan on 4/3/15.
+//  Copyright (c) 2015 Flyby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
