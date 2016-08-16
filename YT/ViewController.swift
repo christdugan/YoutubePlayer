1a1599b6510677d8c08288c143d0e7c96dbe842e
Chris Dugan <christophertdugan@gmail.com>
2016-08-16 18:35:21 -0400

    first commit

--- YT/ViewController.swift
+++ YT/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, YTPlayerViewDelegate {
+    
+    @IBOutlet weak var preview: UIImageView!
+    @IBOutlet weak var player: YTPlayerView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var actInd : UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        player.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        setUpView()
+    }
 
+    func setUpView() {
+        preview.image = UIImage(named: "bill-preview") //should load preview from cms
+    }
+    
+    func runActivityIndicator() {
+        actInd.hidesWhenStopped = true
+        playButton.hidden = true
+        actInd.startAnimating()
+    }
+
+    @IBAction func playVideo(sender: AnyObject) {
+        runActivityIndicator()
+        player.loadWithVideoId("N9XKLqGqwLA") //should be parse and load func
+    }
+    
+    func playerViewDidBecomeReady(playerView: YTPlayerView!) {
+        player.playVideo()
+    }
+    
+    func playerView(playerView: YTPlayerView!, didChangeToState state: YTPlayerState) {
+        switch state {
+            case .Ended:
+                NSLog("Ended Playback")
+            case .Playing:
+                NSLog("Started Playback")
+                actInd.stopAnimating()
+                playButton.hidden = false
+            default:
+                NSLog("Who CARES")
+        }
+    }
 }
 
